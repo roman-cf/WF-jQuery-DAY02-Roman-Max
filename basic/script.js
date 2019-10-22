48d4e31def12312ea5e2630795ed6c38c05ae5d2
roman.cf <roman.springer@outlook.com>
2019-10-22 09:56:34 +0200

    switch

--- basic/script.js
+++ basic/script.js
@@ -1,3 +1,18 @@
 $("body").append("<img src='./img/1-2-christmas-bell-png-image.png'>");
 $("body").append("<img src='./img/santa-claus.png'>");
 $("body").append("<img src='./img/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.webp'>");
+
+$("img").css({"width":"33%", "height":"auto"});
+
+$("img").on("click", function(){
+	($(this).attr("src");
+
+	switch(($(this).attr("src")){
+		case: 
+
+
+	}		
+
+
+		
+} )
