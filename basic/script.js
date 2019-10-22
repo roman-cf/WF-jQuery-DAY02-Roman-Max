4504e9b19b2a95e39df0349e88e58fa12f63409b
roman.cf <roman.springer@outlook.com>
2019-10-22 09:39:08 +0200

    rauf

---
+++ basic/script.js
@@ -0,0 +1,3 @@
+$("body").append("<img src='./img/1-2-christmas-bell-png-image.png'>");
+$("body").append("<img src='./img/santa-claus.png'>");
+$("body").append("<img src='./img/White_Gift_Box_with_Red_Bow_PNG_Image_2048x.webp'>");
